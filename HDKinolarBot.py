7fa7063aabea331ac531f05befbc1edc6286cba5
Muhammadamin <145181015+Muhammadamin-001@users.noreply.github.com>
2025-11-21 23:44:30 +0500

    Bot

--- HDKinolarBot.py
+++ HDKinolarBot.py
@@ -3,12 +3,14 @@ from telebot import types
 import json
 import time
 import requests
+from flask import Flask, request
 import os
 
-# TOKEN = '8355160126:AAEl-Ul_QLI1I7AwkHPfE1_r4a2MUC0MISU'    #- Bu kerak emas ekan render com bazasi uchun
+#TOKEN = '8355160126:AAEl-Ul_QLI1I7AwkHPfE1_r4a2MUC0MISU'    - Bu kerak emas ekan render com bazasi uchun
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv("TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 CHANNEL_ID = [-1001574709061,  -1003359940811]#Usavyb
 
@@ -16,6 +18,7 @@ bot = telebot.TeleBot(TOKEN)
 
 kanal_link="https://t.me/DubHDkinolar"
 
+app = Flask(__name__)
 
 # ====================== DB YUKLASH ============================
 try:
@@ -500,21 +503,24 @@ def universal_handler(msg):
 
 
 
-while True:
-    try:
-        bot.polling(non_stop=True, timeout=60, long_polling_timeout=60)
-    except requests.exceptions.ReadTimeout:
-        print("⚠️ ReadTimeout — bot qayta ishga tushmoqda...")
-        time.sleep(1)
-        continue
-    except requests.exceptions.ConnectionError:
-        print("⚠️ ConnectionError — internet uzildi. Qayta urinish...")
-        time.sleep(2)
-        continue
-    except Exception as e:
-        print("❗ Noma'lum xatolik:", e)
-        time.sleep(2)
-        continue
+
+@app.route('/' + TOKEN, methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "ok", 200
+
+# Just to test server
+@app.route('/')
+def index():
+    return "Bot is running"
+
+if __name__ == "__main__":
+
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
+
 # ==============================================================#
     
     
